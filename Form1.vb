5e7298e20a8e2624b8631dc8ea753efad65e9c9e
imrius <rius.imc@gmail.com>
2024-06-18 13:27:17 -0300

    LOGICA

--- Form1.vb
+++ Form1.vb
@@ -4,5 +4,16 @@
         Dim num, posMayor As Integer
         Dim suma As Integer = 0
         Dim mayor As Integer = 0
+        For cuenta = 1 To 20 Step 1
+            num = rnd.Next(1, 51)
+            If num Mod 2 = 0 Then
+                suma = suma + 1
+            End If
+            If num > mayor Then
+                mayor = num
+                posMayor = cuenta
+            End If
+            ListBox1.Items.Add(num)
+        Next
     End Sub
 End Class
